e3b314947005cb46cceee88ed3e08c5fe9f32610
balatuluva <bala.tulva@gmail.com>
2023-10-23 07:23:35 +0100

    frontend, mongo, catalogue corrections

--- catalogue.sh
+++ catalogue.sh
@@ -1,15 +1,38 @@
+echo -e "\e[36m>>>>>> Configure Nodejs repos <<<<<<\e[0m"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+
+echo -e "\e[36m>>>>>> Install Nodejs <<<<<<\e[0m"
 yum install nodejs -y
+
+echo -e "\e[36m>>>>>> Add Application user <<<<<<\e[0m"
 useradd roboshop
+
+echo -e "\e[36m>>>>>> Create Application Directory <<<<<<\e[0m"
 mkdir /app
+
+echo -e "\e[36m>>>>>> Download App Content <<<<<<\e[0m"
 curl -L -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
 cd /app
+
+echo -e "\e[36m>>>>>> Unzip App Content <<<<<<\e[0m"
 unzip /tmp/catalogue.zip
+
+echo -e "\e[36m>>>>>> Install Nodejs Dependencies <<<<<<\e[0m"
 npm install
+
+echo -e "\e[36m>>>>>> Copy Catalogue SystemD file <<<<<<\e[0m"
 cp catalogue.service /etc/systemd/system/catalogue.service
+
+echo -e "\e[36m>>>>>> Start Catalogue Service <<<<<<\e[0m"
 systemctl daemon-reload
 systemctl enable catalogue
 systemctl start catalogue
+
+echo -e "\e[36m>>>>>> Copy Mongodb repo <<<<<<\e[0m"
 cp mongo.repo /etc/yum.repos.d/mongo.repo
+
+echo -e "\e[36m>>>>>> Install Mongodb Client <<<<<<\e[0m"
 yum install mongodb-org-shell -y
-mongo --host MONGODB-SERVER-IPADDRESS </app/schema/catalogue.js
+
+echo -e "\e[36m>>>>>> Load Schema <<<<<<\e[0m"
+mongo --host mongodb-dev.gehana26.online </app/schema/catalogue.js
