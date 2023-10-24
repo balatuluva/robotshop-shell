7e5688b5e65d39bc14c08b60207db9d10e6db7b8
balatuluva <bala.tulva@gmail.com>
2023-10-24 06:47:35 +0100

    user repo changes

--- user.sh
+++ user.sh
@@ -1,15 +1,39 @@
+echo -e "\e[36m>>>>>> Download Nodejs repos <<<<<<\e[0m"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+
+echo -e "\e[36m>>>>>> Install Nodejs <<<<<<\e[0m"
 yum install nodejs -y
+
+echo -e "\e[36m>>>>>> Add roboshop user <<<<<<\e[0m"
 useradd roboshop
+
+echo -e "\e[36m>>>>>> Create App Directory <<<<<<\e[0m"
+rm -rf /app
 mkdir /app
+
+echo -e "\e[36m>>>>>> Download user config repos <<<<<<\e[0m"
 curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip
 cd /app
+
+echo -e "\e[36m>>>>>> Unzip user content <<<<<<\e[0m"
 unzip /tmp/user.zip
+
+echo -e "\e[36m>>>>>> Install app dependencies <<<<<<\e[0m"
 npm install
-cp user.service /etc/systemd/system/user.service
+
+echo -e "\e[36m>>>>>> Copy User service files <<<<<<\e[0m"
+cp /home/centos/robotshop-shell/user.service /etc/systemd/system/user.service
+
+echo -e "\e[36m>>>>>> Start user service <<<<<<\e[0m"
 systemctl daemon-reload
 systemctl enable user
 systemctl start user
-cp mongo.repo /etc/yum.repos.d/mongo.repo
+
+echo -e "\e[36m>>>>>> Copy mongo service files <<<<<<\e[0m"
+cp /home/centos/robotshop-shell/mongo.repo /etc/yum.repos.d/mongo.repo
+
+echo -e "\e[36m>>>>>> Install mongo repos <<<<<<\e[0m"
 yum install mongodb-org-shell -y
-mongo --host MONGODB-SERVER-IPADDRESS </app/schema/catalogue.js
+
+echo -e "\e[36m>>>>>> Load Schema <<<<<<\e[0m"
+mongo --host mongodb-dev.gehana26.online </app/schema/catalogue.js
