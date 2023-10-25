502954ef9df8fd6553e69a5fa750d34c5585af86
balatuluva <bala.tulva@gmail.com>
2023-10-25 06:01:55 +0100

    dispatch service

--- dispatch.sh
+++ dispatch.sh
@@ -1,13 +1,29 @@
+echo -e "\e[36m>>>>>> Install golang repos <<<<<<\e[0m"
 yum install golang -y
+
+echo -e "\e[36m>>>>>> Add roboshop user <<<<<<\e[0m"
 useradd roboshop
+
+echo -e "\e[36m>>>>>> Create App directory <<<<<<\e[0m"
+rm -rf /app
 mkdir /app
-cd /app
+
+echo -e "\e[36m>>>>>> Download dispatch content <<<<<<\e[0m"
 curl -L -o /tmp/dispatch.zip https://roboshop-artifacts.s3.amazonaws.com/dispatch.zip
+cd /app
+
+echo -e "\e[36m>>>>>> Unzip dispatch content <<<<<<\e[0m"
 unzip /tmp/dispatch.zip
+
+echo -e "\e[36m>>>>>> Install app dependencies <<<<<<\e[0m"
 go mod init dispatch
 go get
 go build
-cp dispatch.service /etc/systemd/system/dispatch.service
+
+echo -e "\e[36m>>>>>> Copy dispatch service files <<<<<<\e[0m"
+cp /home/centos/robotshop-shell/dispatch.service /etc/systemd/system/dispatch.service
+
+echo -e "\e[36m>>>>>> Restart dispatch service <<<<<<\e[0m"
 systemctl daemon-reload
 systemctl enable dispatch
-systemctl start dispatch
+systemctl restart dispatch
