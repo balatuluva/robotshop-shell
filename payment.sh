ca9f9f138954b3e326208744f33dd55e87d743e8
balatuluva <bala.tulva@gmail.com>
2023-10-25 06:47:21 +0100

    common file and sourcing

--- payment.sh
+++ payment.sh
@@ -1,8 +1,10 @@
+source common.sh
+
 echo -e "\e[36m>>>>>> Intsall python repos <<<<<<\e[0m"
 yum install python36 gcc python3-devel -y
 
 echo -e "\e[36m>>>>>> Add roboshop user <<<<<<\e[0m"
-useradd roboshop
+useradd ${app_user}
 
 echo -e "\e[36m>>>>>> Add app directory <<<<<<\e[0m"
 rm -rf /app
