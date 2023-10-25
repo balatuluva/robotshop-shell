7bfcd8bac93b2fcf51adb8b0c7f09f45f8b96a7b
balatuluva <bala.tulva@gmail.com>
2023-10-25 05:55:27 +0100

    payment service

--- payment.sh
+++ payment.sh
@@ -1,11 +1,27 @@
+echo -e "\e[36m>>>>>> Intsall python repos <<<<<<\e[0m"
 yum install python36 gcc python3-devel -y
+
+echo -e "\e[36m>>>>>> Add roboshop user <<<<<<\e[0m"
 useradd roboshop
+
+echo -e "\e[36m>>>>>> Add app directory <<<<<<\e[0m"
+rm -rf /app
 mkdir /app
-cd /app
+
+echo -e "\e[36m>>>>>> Download payment content <<<<<<\e[0m"
 curl -L -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment.zip
+cd /app
+
+echo -e "\e[36m>>>>>> Unzip payment content <<<<<<\e[0m"
 unzip /tmp/payment.zip
+
+echo -e "\e[36m>>>>>> Install dependencies <<<<<<\e[0m"
 pip3.6 install -r requirements.txt
-cp payment.service /etc/systemd/system/payment.service
+
+echo -e "\e[36m>>>>>> Copy payment service files <<<<<<\e[0m"
+cp /home/centos/robotshop-shell/payment.service /etc/systemd/system/payment.service
+
+echo -e "\e[36m>>>>>> Restart payment service <<<<<<\e[0m"
 systemctl daemon-reload
 systemctl enable payment
-systemctl start payment
+systemctl restart payment
