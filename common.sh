9db5bcfbebb109e3d098bcb80e8528d1b1204c98
balatuluva <bala.tulva@gmail.com>
2023-10-31 07:01:39 +0000

    java func, schema setup func, systemd setup func and mysql in schema setup

--- common.sh
+++ common.sh
@@ -2,54 +2,86 @@ app_user=roboshop
 script=$(realpath "$0")
 script_path=$(dirname "$script")
 
-print_head () {
+func_print_head() {
   echo -e "\e[36m>>>>>> $1 <<<<<<\e[0m"
 }
 
-schema_setup() {
+func_app_prereq() {
+  func_print_head "Add App user"
+  useradd ${app_user}
+
+  func_print_head "Create app directory"
+  rm -rf /app
+  mkdir /app
+
+  func_print_head "Download App content"
+  curl -L -o /tmp/{component}.zip https://roboshop-artifacts.s3.amazonaws.com/{component}.zip
+  cd /app
+
+  func_print_head "Unzip App content"
+  unzip /tmp/{component}.zip
+}
+
+func_systemd_setup() {
+  func_print_head "copy {component} service"
+  cp ${script_path}/{component}.service /etc/systemd/system/{component}.service
+
+  func_print_head "Start {component} service"
+  systemctl daemon-reload
+  systemctl enable {component}
+  systemctl start {component}
+}
+
+func_schema_setup() {
   if [ "$schema_setup" == "mongo" ]; then
-    print_head "Copy Mongodb repo"
+    func_print_head "Copy Mongodb repo"
     cp ${script_path}/mongo.repo /etc/yum.repos.d/mongo.repo
 
-    print_head "Install Mongodb Client"
+    func_print_head "Install Mongodb Client"
     yum install mongodb-org-shell -y
 
-    print_head "Load Schema"
+    func_print_head "Load Schema"
     mongo --host mongodb-dev.gehana26.online </app/schema/{component}.js
+  fi
+  if [ "$schema_setup" == "mysql" ]; then
+    func_print_head "Install Mysql"
+    yum install mysql -y
+
+    func_print_head "Load Schema"
+    mysql -h mysql-dev.gehana26.online -uroot -p${mysql_root_password} < /app/schema/shipping.sql
   fi
 }
 
 func_nodejs() {
-  print_head "Download Nodejs"
+  func_print_head "Download Nodejs"
   curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 
-  print_head "Install Nodejs"
+  func_print_head "Install Nodejs"
   yum install nodejs -y
 
-  print_head "Add roboshop user"
-  useradd ${app_user}
+  func_app_prereq
 
-  print_head "Create app directory"
-  mkdir /app
-
-  print_head "Download cart content"
-  curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip
-  cd /app
-
-  print_head "Unzip cart content"
-  unzip /tmp/${component}.zip
-
-  print_head "Install dependencies"
+  func_print_head "Install dependencies"
   npm install
 
-  print_head "Create App Directory"
-  cp ${script_path}/${component}.service /etc/systemd/system/${component}.service
+  func_schema_setup
 
-  print_head "Start cart service"
-  systemctl daemon-reload
-  systemctl enable ${component}
-  systemctl restart ${component}
+  func_systemd_setup
+}
 
-  print_head "Schema setup function"
-  schema_setup
+func_java() {
+  func_print_head "Install maven"
+  yum install maven -y
+
+  func_app_prereq
+
+  func_print_head "Install Maven dependencies"
+  mvn clean package
+
+  func_print_head "Download {component} service"
+  mv target/{component}-1.0.jar {component}.jar
+
+  func_schema_setup
+
+  func_systemd_setup
 }
--- shipping.sh
+++ shipping.sh
@@ -8,39 +8,6 @@ if [ -z "$mysql_root_password" ]; then
   exit
 fi
 
-echo -e "\e[36m>>>>>> Install maven <<<<<<\e[0m"
-yum install maven -y
-
-echo -e "\e[36m>>>>>> Add roboshop user <<<<<<\e[0m"
-useradd ${app_user}
-
-echo -e "\e[36m>>>>>> Create app directory <<<<<<\e[0m"
-rm -rf /app
-mkdir /app
-
-echo -e "\e[36m>>>>>> Download shipping content <<<<<<\e[0m"
-curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping.zip
-cd /app
-
-echo -e "\e[36m>>>>>> Unzip shipping content <<<<<<\e[0m"
-unzip /tmp/shipping.zip
-
-echo -e "\e[36m>>>>>> Install Maven dependencies <<<<<<\e[0m"
-mvn clean package
-
-echo -e "\e[36m>>>>>> Download shipping service <<<<<<\e[0m"
-mv target/shipping-1.0.jar shipping.jar
-
-echo -e "\e[36m>>>>>> copy shipping service <<<<<<\e[0m"
-cp ${script_path}/shipping.service /etc/systemd/system/shipping.service
-
-echo -e "\e[36m>>>>>> Install Mysql <<<<<<\e[0m"
-yum install mysql -y
-
-echo -e "\e[36m>>>>>> Load Schema <<<<<<\e[0m"
-mysql -h mysql-dev.gehana26.online -uroot -p${mysql_root_password} < /app/schema/shipping.sql
-
-echo -e "\e[36m>>>>>> Start shipping service <<<<<<\e[0m"
-systemctl daemon-reload
-systemctl enable shipping
-systemctl start shipping
+component="shipping"
+func_schema_setup=mysql
+func_java
