85f7f96ce243f71f82d0e3277661cea6c1090fe8
balatuluva <bala.tulva@gmail.com>
2023-11-03 06:27:48 +0000

    Code DRY for mysql service

--- mysql.sh
+++ mysql.sh
@@ -9,17 +9,22 @@ if [ -z "$mysql_root_password" ]; then
 fi
 
 func_print_head "Disable default Mysql version"
-dnf module disable mysql -y
+dnf module disable mysql -y &>>$log_file
+func_status_check $?
 
 func_print_head "Load Schema"
-cp ${script_path}/mysql.repo /etc/yum.repos.d/mysql.repo
+cp ${script_path}/mysql.repo /etc/yum.repos.d/mysql.repo &>>$log_file
+func_status_check $?
 
 func_print_head "Install Mysql"
-yum install mysql-community-server -y
+yum install mysql-community-server -y &>>$log_file
+func_status_check $?
 
 func_print_head "start mysql"
-systemctl enable mysqld
-systemctl restart mysqld
+systemctl enable mysqld &>>$log_file
+systemctl restart mysqld &>>$log_file
+func_status_check $?
 
 func_print_head "Set Mysql password"
-mysql_secure_installation --set-root-pass $mysql_root_password
+mysql_secure_installation --set-root-pass $mysql_root_password &>>$log_file
+func_status_check $?
