c40f03d207127bfb18c9203bcdee248f06f5b0ce
balatuluva <bala.tulva@gmail.com>
2023-11-04 13:48:58 +0000

    dispatch DRY code

--- dispatch.sh
+++ dispatch.sh
@@ -6,23 +6,7 @@ func_print_head "Install golang repos"
 yum install golang -y &>>$log_file
 func_status_check $?
 
-func_print_head "Add roboshop user"
-useradd roboshop &>>$log_file
-func_status_check $?
-
-func_print_head "Create App directory"
-rm -rf /app &>>$log_file
-mkdir /app &>>$log_file
-func_status_check $?
-
-func_print_head "Download dispatch content"
-curl -L -o /tmp/dispatch.zip https://roboshop-artifacts.s3.amazonaws.com/dispatch.zip &>>$log_file
-cd /app
-func_status_check $?
-
-func_print_head "Unzip dispatch content"
-unzip /tmp/dispatch.zip &>>$log_file
-func_status_check $?
+func_app_prereq
 
 func_print_head "Install app dependencies"
 go mod init dispatch &>>$log_file
@@ -30,12 +14,4 @@ go get &>>$log_file
 go build &>>$log_file
 func_status_check $?
 
-func_print_head "Copy dispatch service files"
-cp ${script_path}/dispatch.service /etc/systemd/system/dispatch.service &>>$log_file
-func_status_check $?
-
-func_print_head "Restart dispatch service"
-systemctl daemon-reload &>>$log_file
-systemctl enable dispatch &>>$log_file
-systemctl restart dispatch &>>$log_file
-func_status_check $?
+func_systemd_setup
